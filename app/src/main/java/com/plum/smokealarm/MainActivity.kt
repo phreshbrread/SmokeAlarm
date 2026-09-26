1e32d13ae87145720f2bd13b1310f84aead95e0b
PhreshBrread <crustydog9@gmail.com>
2026-09-26 12:45:25 +1000

    Clean up imports

--- app/src/main/java/com/plum/smokealarm/MainActivity.kt
+++ app/src/main/java/com/plum/smokealarm/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.plum.smokealarm
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.media.SoundPool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,24 +15,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import com.plum.smokealarm.ui.theme.SmokeAlarmTheme
-import androidx.compose.material3.Switch
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import android.media.AudioAttributes
-import android.media.MediaPlayer
-import android.media.SoundPool
-import android.media.audiofx.LoudnessEnhancer
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.plum.smokealarm.ui.theme.SmokeAlarmTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
